4d12fe8b436c678d220137a2b83453ea5e95e97c
Troy Morehouse <troymore@nbnet.nb.ca>
2017-04-12 16:45:19 -0300

    Refactored for Vue 2+
    Converted to Vue v2.+
    
    Added modifier prepend (default is append) to specify the position in the target element

--- index.js
+++ index.js
@@ -10,40 +10,23 @@
 }(this, function (Vue, options) {
 
 var name = (options && options.name) || 'transferDom'
-name = Vue.util.hyphenate(name)
 
-var FragmentFactory = Vue.FragmentFactory
-var remove = Vue.util.remove
-var createAnchor = Vue.util.createAnchor
-
-// register as terminal directive
+// Define the directive
 Vue.directive(name, {
-  priority: 2000,
-  terminal: true,
-  bind: function () {
-    var el = this.el
-    if (!el.__vue__) {
-      // default append to <body>
-      var container = this.arg
-        ? document.getElementById(this.arg)
-        : document.body
-      this.anchor = createAnchor('v-transfer-dom')
-      container.appendChild(this.anchor)
-      remove(this.el)
-      var factory = new FragmentFactory(this.vm, el)
-      this.frag = factory.create(this._host, this._scope, this._frag)
-      this.frag.before(this.anchor)
+  inserted: function (el, bindings, vnode) {
+    // default append to <body>
+    var container = bindings.arg
+      ? document.getElementById(bindings.arg)
+      : document.body
+    if (container) {
+    	( bindings.modifiers.prepend && container.firstChild )
+	    	? container.insertBefore(el, container.firstChild)  // top of target
+        :	container.appendChild(el)                         // bottom of target
     } else {
       console.warn(
-        'v-transfer-dom="' + this.expression + '" cannot be ' +
-        'used on an instance root element.'
+        'v-' + name + ' target element id "' + bindings.arg + '" not found.'
       )
     }
-  },
-  unbind: function () {
-    this.frag.remove()
-    remove(this.anchor)
-  }
 })
 
 })
